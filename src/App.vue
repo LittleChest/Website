09c67802c17edb698202b9b49aada6b1d7ccb387
LittleChest <hiya@littlew.top>
2026-02-05 17:30:13 +0800

    app: Add Mobius Internet support

--- src/App.vue
+++ src/App.vue
@@ -204,7 +204,13 @@ function updateShadow() {
         >
           {{ user.name || user.username }}
         </span>
-        <span v-else> 來自「{{ user.asorg || '未至之境' }}」的朋友</span>
+        <span v-else-if="user.asn === 204539 && user.asorg === 'LittleChest - Mobius Internet'"
+          >來自<span
+            class="relative inline-block overflow-hidden text-violet-300 before:absolute before:inset-0 before:z-[-1] before:[animation:slideIn_1s_forwards] before:bg-violet-800 before:[--slideIn:inset_0_auto_auto_0_calc(100%)] md:text-6xl/tight"
+            >「Möbius Internet」</span
+          >的朋友</span
+        >
+        <span v-else>來自「{{ user.asorg || '未至之境' }}」的朋友</span>
         <span>。</span>
       </p>
       <div class="w-3/5 pt-30 text-4xl font-medium md:text-6xl">
